2af2ebcf9787926059decaf4b14aaa01b83eb343
rifrif14 <rifka1412@gmail.com>
2021-09-30 10:40:48 +0700

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,6 +1,6 @@
 variable "region" {
   description = "AWS region"
-  default     = "us-west-1"
+  default     = "us-southeast-1"
 }
 
 variable "instance_type" {
@@ -10,6 +10,6 @@ variable "instance_type" {
 
 variable "instance_name" {
   description = "EC2 instance name"
-  default     = "Provisioned by Terraform"
+  default     = "Provisioned by Terraform Cloud"
 }
 
